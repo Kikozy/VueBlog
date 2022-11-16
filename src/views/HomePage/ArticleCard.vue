5a8f5ae2875c9a627cadb094711cecb1a9671da9
Kikozy <yuexuanxuanw@gmail.com>
2022-11-16 22:46:30 +0800

    修改:文章卡片样式修改

--- src/views/HomePage/ArticleCard.vue
+++ src/views/HomePage/ArticleCard.vue
@@ -1,20 +1,20 @@
 <script setup lang="ts">
-import 'highlight.js/styles/foundation.css'
-import { ArticleCardStruc } from '@api/query'
-import TagItem from '@comp/TagItem.vue'
-import router from '../../router/index'
-import { onMounted, ref } from 'vue'
-import { queryIssuesList } from '@api/query'
+import "highlight.js/styles/foundation.css"
+import { ArticleCardStruc } from "@api/query"
+import TagItem from "@comp/TagItem.vue"
+import router from "../../router/index"
+import { onMounted, ref } from "vue"
+import { queryIssuesList } from "@api/query"
 
 const props = defineProps<{
-  articleCardState: ArticleCardStruc
+	articleCardState: ArticleCardStruc
 }>()
 
 const cardShow = ref(false)
 
 //打开详情
 function hendleOpenArticle(cardNum: number) {
-  router.push({ path: `/ArticleDetails/${cardNum}` })
+	router.push({ path: `/ArticleDetails/${cardNum}` })
 }
 
 /**
@@ -22,67 +22,101 @@ function hendleOpenArticle(cardNum: number) {
  * @param tagName tag的名字
  */
 function handelChooseTag(tagName: string) {
-  queryIssuesList({ labels: tagName })
+	queryIssuesList({ labels: tagName })
 }
 onMounted(() => {
-  cardShow.value = true
+	cardShow.value = true
 })
 </script>
 
 <template>
-  <transition name="article-card">
-    <div class="article-card" v-if="cardShow">
-      <header>
-        <p class="card-title" @click="hendleOpenArticle(props.articleCardState.number)">
-          标题: {{ props.articleCardState.title }}
-        </p>
-      </header>
-      <footer>
-        <p class="card-info">
-          创建时间: {{ props.articleCardState.created_at }} 最后修改: {{ props.articleCardState.updated_at }}
-          <tag-item
-            v-for="tag of props.articleCardState.labels"
-            @click="handelChooseTag(tag.name)"
-            :key="tag.name"
-            :color="tag.color"
-          >
-            {{ tag.name }}
-          </tag-item>
-          分类: {{ props.articleCardState.milestone.title }}
-        </p>
-      </footer>
-    </div>
-  </transition>
+	<transition name="article-card">
+		<div class="article-card" v-if="cardShow">
+			<div class="card-img">
+				<img src="" alt="" />
+			</div>
+			<!-- 文章信息 -->
+			<div class="article-info">
+				<!-- 文章标题 -->
+				<p class="article-title" @click="hendleOpenArticle(props.articleCardState.number)">
+					标题: {{ props.articleCardState.title }}
+				</p>
+				<!-- 文章时间 -->
+				<p class="article-time">
+					创建时间: {{ new Date(props.articleCardState.created_at).toLocaleDateString() }} 最后修改:
+					{{ new Date(props.articleCardState.updated_at).toLocaleDateString() }}
+				</p>
+				<!-- 文章标签 -->
+				<div class="article-tags">
+					<tag-item
+						v-for="tag of props.articleCardState.labels"
+						@click="handelChooseTag(tag.name)"
+						:key="tag.name"
+						:color="tag.color"
+					>
+						{{ tag.name }}
+					</tag-item>
+				</div>
+				<!-- 文章分类 -->
+				<div class="article-sort">分类: {{ props.articleCardState.milestone.title }}</div>
+			</div>
+		</div>
+	</transition>
 </template>
 
 <style lang="scss" scoped>
 .article-card {
-	padding: 20px 0;
-  margin-bottom: 5vh;
-  border: 2px solid $baseBorderColor;
-  box-shadow: 0 1px 2px -2px rgba(0, 0, 0, 0.24), 0 3px 6px 0 rgba(0, 0, 0, 0.18), 0 5px 12px 4px rgba(0, 0, 0, 0.12);
-  .card-title {
-    color: $baseTextColor;
-    cursor: pointer;
-    &:hover {
-      color: $baseHoverColor;
+	width: 30%;
+	max-width: 380px;
+	height: 300px;
+	margin-bottom: 5vh;
+	border: 2px solid $baseBorderColor;
+	box-shadow: 0 1px 2px -2px rgba(0, 0, 0, 0.24), 0 3px 6px 0 rgba(0, 0, 0, 0.18), 0 5px 12px 4px rgba(0, 0, 0, 0.12);
+	.card-img {
+		width: 100%;
+		height: 200px;
+		background-color: pink;
+	}
+	.article-info {
+		display: flex;
+		flex-direction: column;
+		.article-title {
+			font-size: 20px;
+		}
+		.article-time {
+			font-size: 14px;
+		}
+    .article-tags {
+      
     }
-  }
+    .article-sort{
+      font-size: 14px;
+    }
+	}
+	.card-title {
+		box-sizing: border-box;
+		padding: 0 10px;
+		color: $baseTextColor;
+		cursor: pointer;
+		&:hover {
+			color: $baseHoverColor;
+		}
+	}
 }
 
 .article-card-enter-active {
-  animation: cardIn 1.5s ease;
+	animation: cardIn 1.5s ease;
 }
 
 @keyframes cardIn {
-  0% {
-    opacity: 0;
-    transform: translateY(100px);
-  }
+	0% {
+		opacity: 0;
+		transform: translateY(100px);
+	}
 
-  100% {
-    opacity: 1;
-    transform: translateY(0);
-  }
+	100% {
+		opacity: 1;
+		transform: translateY(0);
+	}
 }
 </style>
--- src/views/HomePage/HomePage.vue
+++ src/views/HomePage/HomePage.vue
@@ -4,32 +4,38 @@ import { queryIssuesList, ArticleCardStruc } from "@api/query"
 import ArticleCard from "./ArticleCard.vue"
 
 const state = reactive({
-  cardList: <Array<ArticleCardStruc>>[],
-  pageLoading: true,
+	cardList: <Array<ArticleCardStruc>>[],
+	pageLoading: true,
 })
 
+
 onMounted(() => {
-  //页面挂载完毕
+	//页面挂载完毕
 
-  //1. 展示载入动画，等待接口数据的返回
+	//1. 展示载入动画，等待接口数据的返回
 
-  queryIssuesList().then((res: any) => {
-    state.cardList = res
-  })
+	queryIssuesList().then((res: any) => {
+		state.cardList = res
+	})
 })
 </script>
 
 <template>
-  <div class="article-list">
-    <article-card v-for="cardItem of state.cardList" :article-card-state="cardItem" :key="cardItem.number" />
-  </div>
+	<div class="article-list">
+		<article-card v-for="cardItem of state.cardList" :article-card-state="cardItem" :key="cardItem.number" />
+	</div>
 </template>
 
 <style lang="scss">
 .article-list {
-  position: relative;
-  width: 100%;
-  height: 100%;
-  background-color: rgb(24, 24, 28);
+  box-sizing: border-box;
+  padding: 20px;
+	display: flex;
+  flex-wrap: wrap;
+	gap: 20px;
+	position: relative;
+	width: 100%;
+	height: 100%;
+	background-color: rgb(24, 24, 28);
 }
 </style>
